cecb973ece69f2dbaee630a006b023b45786558a
hidmd03 <39095064+hidmd03@users.noreply.github.com>
2020-02-28 16:48:24 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="wrap">
+      <div class="img_wrap">
+        <div class="p1"></div>
+        <div class="p2"></div>
+        <div class="p3"></div>
+        <div class="p4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
